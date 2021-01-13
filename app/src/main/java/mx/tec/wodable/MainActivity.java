7d61d3c96f8abbda43b95035fb857072231d095e
Renegal1238 <a01654359@itesm.mx>
2021-01-13 09:23:15 -0600

    New

--- app/src/main/java/mx/tec/wodable/MainActivity.java
+++ app/src/main/java/mx/tec/wodable/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     // shared prefs
     private SharedPreferences prefs;
 
-    Button user;
+    Button user, myprofile;
     TextView saludo;
 
     @Override
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         user = findViewById(R.id.userinfo);
         saludo = findViewById(R.id.saludohomepage);
+        myprofile = findViewById(R.id.myprofilebutton);
 
         saludo.setText(prefs.getString(NOMBRE_KEY, " ") + " " + prefs.getString(APELLIDO_KEY, " "));
 
@@ -52,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
         // Esperamos retorno
         startActivityForResult(user, USERINFO_CODE);
     }
+
+    public void myProfile(View v){
+
+        Intent myprofile = new Intent(this, ProfileInfoActivity.class);
+
+        startActivity(myprofile);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -69,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                     prefs.getString(NOMBRE_KEY, "No hay saludo") + " " +
                     prefs.getString(APELLIDO_KEY, "No hay saludo"), Toast.LENGTH_SHORT).show();
         }
+        saludo.setText(prefs.getString(NOMBRE_KEY, " ") + " " + prefs.getString(APELLIDO_KEY, " "));
     }
 
 }
--- app/src/main/java/mx/tec/wodable/ProfileInfoActivity.java
+++ app/src/main/java/mx/tec/wodable/ProfileInfoActivity.java
@@ -2,13 +2,29 @@ package mx.tec.wodable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileInfoActivity extends AppCompatActivity {
+
+    private static final String ARCHIVO_PREFS = "prefs";
+    private static final String NOMBRE_KEY = "usernombre";
+    private static final String APELLIDO_KEY = "userapellido";
+    private SharedPreferences prefs;
+
+    TextView Saludo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
+
+        Saludo = findViewById(R.id.textView2);
+
+        prefs = getSharedPreferences(ARCHIVO_PREFS, MODE_PRIVATE);
+
+        Saludo.setText(prefs.getString(NOMBRE_KEY, " ") + " " + prefs.getString(APELLIDO_KEY, " "));
+
     }
 }
