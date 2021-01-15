c62dfd0ce466489cbaacaf3c5129fb317b8f8ffa
Soul99 <47033830+Alma-Anguiano@users.noreply.github.com>
2021-01-14 23:32:29 -0600

    actividad informacion de perfil de usuario

---
+++ app/src/main/java/mx/tec/wodable/edit_info_personal.java
@@ -0,0 +1,14 @@
+package mx.tec.wodable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_info_personal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_info_personal);
+    }
+}
